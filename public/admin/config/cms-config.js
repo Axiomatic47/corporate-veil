cb450f0d1dd69a51ca47933f4670681b640df6ad
Axiomatic47 <joeykd47@gmail.com>
2025-01-13 08:17:02 -0600

    New Section button fix with navigation menu.

--- public/admin/config/cms-config.js
+++ public/admin/config/cms-config.js
@@ -20,15 +20,18 @@ const cmsConfig = {
         folder: 'content/compositions',
         create: true,
         slug: '{{slug}}',
+        editor: {
+          preview: false
+        },
         fields: [
-          { 
+          {
             label: 'Collection Type',
             name: 'collection_type',
             widget: 'select',
             options: ['memorandum', 'corrective'],
             required: true
           },
-          { 
+          {
             label: 'Section',
             name: 'section',
             widget: 'number',
@@ -36,56 +39,46 @@ const cmsConfig = {
             min: 1,
             required: true
           },
-          { 
-            label: 'Previous Section Title',
-            name: 'previous_section',
-            widget: 'relation',
-            collection: 'compositions',
-            search_fields: ['title'],
-            value_field: 'title',
-            display_fields: ['title'],
-            required: false
-          },
           { label: 'Title', name: 'title', widget: 'string' },
           { label: 'Description', name: 'description', widget: 'text' },
-          { 
+          {
             label: 'Reading Level',
             name: 'reading_level',
             widget: 'select',
             options: ['1', '3', '5'],
             required: true
           },
-          { 
+          {
             label: 'Content Level 1',
             name: 'content_level_1',
-            widget: 'text',
+            widget: 'markdown',
             required: true
           },
-          { 
+          {
             label: 'Content Level 3',
             name: 'content_level_3',
-            widget: 'text',
+            widget: 'markdown',
             required: true
           },
-          { 
+          {
             label: 'Content Level 5',
             name: 'content_level_5',
-            widget: 'text',
+            widget: 'markdown',
             required: true
           },
-          { 
+          {
             label: 'Title Level 1',
             name: 'title_level_1',
             widget: 'string',
             required: true
           },
-          { 
+          {
             label: 'Title Level 3',
             name: 'title_level_3',
             widget: 'string',
             required: true
           },
-          { 
+          {
             label: 'Title Level 5',
             name: 'title_level_5',
             widget: 'string',
@@ -106,9 +99,7 @@ const cmsConfig = {
           }
         ]
       }
-    ],
-    display_url: window.location.origin,
-    site_url: window.location.origin
+    ]
   }
 };
 
